3fbc12f0eaceb03159e3bc41cd3f0edce08429be
UserOfTheSite <dorsa_dorsa76@yahoo.com>
2023-02-03 12:11:19 +0100

    add category func

--- app/Dashboard.tsx
+++ app/Dashboard.tsx
@@ -1,13 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import React, {
-  ChangeEvent,
-  SyntheticEvent,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { supabase } from '../lib/subpabaseClient';
 import { Session, User } from '@supabase/supabase-js';
 
@@ -20,13 +14,25 @@ const Dashboard = ({
 }) => {
   console.log(user?.id);
   const [imageUrls, setImageUrls] = useState<string[] | null>(null);
+  const [categories, setCategories] = useState<string[] | null>(null);
   const fileRef = useRef<HTMLInputElement | null>(null);
 
+  const [category, setCategory] = useState<null | string>(null);
+  const categoryRef = useRef<HTMLInputElement | null>(null);
+
   const getAllImagesFromDB = async () => {
+    const { data: cats, error: error1 } = await supabase
+      .from('category')
+      .select('*')
+      .eq('name', category);
+
+    if (!cats || cats.length === 0) return;
+
     const { data, error } = await supabase
       .from('image')
       .select('*')
-      .eq('user_id', user!.id);
+      .eq('user_id', user!.id)
+      .eq('category_id', cats[0].id);
 
     if (!data) return;
     console.log(data);
@@ -34,9 +40,22 @@ const Dashboard = ({
     setImageUrls(data.map(img => img.url));
   };
 
+  const getAllCategoriesFromDB = async () => {
+    const { data, error } = await supabase.from('category').select('*');
+
+    if (!data) return;
+
+    setCategories(data.map(cat => cat.name));
+  };
+
+  useEffect(() => {
+    // getAllImagesFromDB();
+    getAllCategoriesFromDB();
+  }, [user?.id]);
+
   useEffect(() => {
     getAllImagesFromDB();
-  }, [user?.id]);
+  }, [category]);
 
   const uploadImageDB = async (image: File | null) => {
     if (!image) return;
@@ -49,9 +68,18 @@ const Dashboard = ({
       .from('memory-bucket')
       .getPublicUrl(`${image.name}`).data.publicUrl;
 
-    const {} = await supabase
-      .from('image')
-      .insert([{ user_id: user!.id, url: url }]);
+    const { data, error } = await supabase
+      .from('category')
+      .select('*')
+      .eq('name', category);
+
+    console.log('this is the cateogryyyyyy' + category + data);
+
+    if (data) {
+      const {} = await supabase
+        .from('image')
+        .insert([{ user_id: user!.id, url: url, category_id: data[0].id }]);
+    }
 
     getAllImagesFromDB();
   };
@@ -62,10 +90,28 @@ const Dashboard = ({
     if (image) uploadImageDB(image);
   };
 
+  const setCategoryHandler = async () => {
+    const {} = await supabase
+      .from('category')
+      .insert([{ name: categoryRef.current!.value }]);
+
+    setCategory(categoryRef.current!.value);
+    getAllCategoriesFromDB();
+  };
+
   return (
     <div>
       <div>
         <h1>Your dashboard</h1>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="category"
+          ref={categoryRef}
+        />
+        <button onClick={setCategoryHandler}>Set Category</button>
+
         <input
           type="file"
           name=""
@@ -76,8 +122,27 @@ const Dashboard = ({
       </div>
       <div>
         <ul>
+          {categories?.map(cat => (
+            <li
+              style={{ cursor: 'pointer' }}
+              key={cat}
+              onClick={() => setCategory(cat)}
+            >
+              {cat}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
           {imageUrls?.map(imageUrl => (
-            <Image src={imageUrl} alt={'picture'} width={800} height={800} />
+            <Image
+              key={imageUrl}
+              src={imageUrl}
+              alt={'picture'}
+              width={800}
+              height={800}
+            />
           ))}
         </ul>
       </div>
