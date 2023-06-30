d8d51aeae07e4c00d733bab3d528388fedd3bf3f
dorsaSz97 <dorsa_dorsa76@yahoo.com>
2023-06-30 23:52:11 +0200

    fixing image problems

--- app/dashboard/[folderName]/ImageGallery.tsx
+++ app/dashboard/[folderName]/ImageGallery.tsx
@@ -1,24 +1,26 @@
-'use client';
-import Image, { ImageProps } from 'next/image';
+"use client";
+import Image, { ImageProps } from "next/image";
 import React, {
   forwardRef,
   MouseEvent,
   useEffect,
   useMemo,
   useState,
-} from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { ImageType } from '@/types';
+} from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { ImageType } from "@/types";
 
 const ExoticImage = forwardRef<HTMLImageElement, ImageProps>(
   function ExoticImageWrapper(props, ref) {
     const [isLoading, setIsLoading] = useState(true);
     const classes = isLoading
-      ? 'scale-110 blur-2xl grayscale'
-      : 'scale-100 blur-0 grayscale-0';
+      ? "scale-110 blur-2xl grayscale"
+      : "scale-100 blur-0 grayscale-0";
 
     return (
       <Image
+        width={2000}
+        height={2000}
         {...props}
         ref={ref}
         onLoadingComplete={() => setIsLoading(false)}
@@ -40,7 +42,7 @@ const ImageGallery = ({ images }: { images: ImageType[] }) => {
 
   return (
     <ul className="grid grid-cols-fluid gap-2 flex-1 justify-center">
-      {images.map(image => (
+      {images.map((image) => (
         <motion.div
           layout={true}
           key={image.id}
@@ -58,14 +60,14 @@ const ImageGallery = ({ images }: { images: ImageType[] }) => {
                 animate={{
                   opacity: 0.9,
                 }}
-                className={`  ${' fixed bg-black  opacity-90 top-0 left-0 z-[1999] w-full h-full'}`}
+                className={`  ${" fixed bg-black  opacity-90 top-0 left-0 z-[1999] w-full h-full"}`}
               />
             )}
           </AnimatePresence>
           <MotionComponent
-            layout={'preserve-aspect'}
+            layout={"preserve-aspect"}
             src={image.url}
-            alt={'A memory'}
+            alt={"A memory"}
             onClick={() => {
               setIsImageZoomed(!isImageZoomed);
               setSelectedImage(image.id);
@@ -74,11 +76,11 @@ const ImageGallery = ({ images }: { images: ImageType[] }) => {
               layout: { duration: 0.3 },
             }}
             className={`object-cover aspect-[1] ${
-              selectedImage === image.id ? ' z-[100000] ' : '  z-[1000] '
+              selectedImage === image.id ? " z-[100000] " : "  z-[1000] "
             }  ${
               isImageZoomed && selectedImage === image.id
-                ? 'fixed w-[700px]  m-auto top-1/2 left-1/2 mt-[-350px] ml-[-350px]  z-[100000] '
-                : 'absolute top-0 left-0 w-[300px]  '
+                ? "fixed w-[700px]  m-auto top-1/2 left-1/2 mt-[-350px] ml-[-350px]  z-[100000] "
+                : "absolute top-0 left-0 w-[300px]  "
             }  cursor-pointer  `}
           />
           {/* <motion.img
