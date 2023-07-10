100f61871ee2f93668d02aae21dc2849544d7ecc
dorsaSz97 <dorsa_dorsa76@yahoo.com>
2023-07-10 19:05:22 +0200

    add edit info func

--- app/dashboard/[folderName]/InfoBtn.tsx
+++ app/dashboard/[folderName]/InfoBtn.tsx
@@ -1,10 +1,26 @@
 "use client";
 import { FolderType } from "@/types";
+import supabase from "@/util/subpabaseClient-browser";
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const InfoBtn = ({ selectedFolder }: { selectedFolder: FolderType }) => {
   const [showInfo, setShowInfo] = useState(false);
+  const [info, setInfo] = useState(selectedFolder.info ?? "");
+  const [edit, setEdit] = useState(false);
+  const paragraphRef = useRef<HTMLParagraphElement | null>(null);
+
+  useEffect(() => {
+    const updateText = async () => {
+      setInfo(paragraphRef.current!.textContent ?? "");
+      const { error } = await supabase
+        .from("folders")
+        .update({ info: paragraphRef.current!.textContent })
+        .eq("id", selectedFolder.id)
+        .eq("user_id", selectedFolder.user_id);
+    };
+    if (paragraphRef.current) updateText();
+  }, [paragraphRef.current?.textContent]);
 
   return (
     <div className="flex justify-center relative font-bold mb-auto">
@@ -14,12 +30,22 @@ const InfoBtn = ({ selectedFolder }: { selectedFolder: FolderType }) => {
       {showInfo && (
         <AnimatePresence>
           <motion.div
-            className="bg-primary absolute gap-8 w-fit items-center left-full bottom-full min-w-[300px] h-[200px] overflow-y-scroll p-3 rounded-[5px] shadow-2xl z-[90000]"
+            className="bg-primary absolute gap-8 w-fit items-center flex flex-col left-[110%] bottom-1/2 min-w-[300px] h-[200px]  overflow-y-scroll scrollbar-hidden p-3 rounded-[5px] shadow-2xl z-[90000]"
             animate={{ opacity: 1 }}
             initial={{ opacity: 0 }}
             exit={{ opacity: 0 }}
           >
-            <p>{selectedFolder.info ?? ""}</p>
+            <p
+              className={`flex-1 w-full ${edit ? "border-2 border-black" : ""}`}
+              contentEditable={edit}
+              ref={paragraphRef}
+              onChange={() => setInfo(paragraphRef.current?.textContent ?? "")}
+            >
+              {info}
+            </p>
+            <button onClick={() => setEdit(!edit)}>
+              {edit ? "Done" : "Edit"}
+            </button>
           </motion.div>
         </AnimatePresence>
       )}
