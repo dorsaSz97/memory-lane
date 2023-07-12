019a2212a1716d6740ce405ef8044bc73b2d4269
dorsaSz97 <dorsa_dorsa76@yahoo.com>
2023-07-12 15:42:31 +0200

    temp fix for the view all image revalidation

--- app/dashboard/[folderName]/ActionsBar.tsx
+++ app/dashboard/[folderName]/ActionsBar.tsx
@@ -1,7 +1,10 @@
-import Link from "next/link";
+"use client";
+import { CursorContext } from "@/components/ui/CursorManager";
 import DeleteButton from "./DeleteButton";
 import { SupaFolder } from "@/types";
 import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
 
 const ActionsBar = ({
   currentFolder,
@@ -10,11 +13,25 @@ const ActionsBar = ({
   currentFolder: SupaFolder;
   user: User;
 }) => {
+  const router = useRouter();
+  const { setMode } = useContext(CursorContext);
   return (
     <div className="flex justify-between items-center">
-      <Link href="/dashboard" className="text-lg underline underline-offset-1">
+      <button
+        onMouseEnter={() => setMode("")}
+        onMouseLeave={() => setMode("normal")}
+        // ::::::::::::::::::::::::::::::::::::::::::NOT WORKING
+        onClick={() => {
+          setMode("normal");
+          router.push("/dashboard");
+          router.refresh();
+        }}
+        // ::::::::::::::::::::::::::::::::::::::::::NOT WORKING
+        // href="/dashboard"
+        className="text-lg underline underline-offset-1"
+      >
         Visit all folders
-      </Link>
+      </button>
       <DeleteButton currentFolder={currentFolder} user={user} />
     </div>
   );
